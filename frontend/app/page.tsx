909f2b10c3e9e7dc9d4b8f8294b3f061efb3db35
arumugasundar <arumugasundar29@gmail.com>
2024-02-07 06:32:48 +0530

    feat : added ui code

---
+++ frontend/app/page.tsx
@@ -0,0 +1,26 @@
+import {Button} from '@/components/ui/button'
+import {LoginButton} from '@/components/auth/login-button'
+export default async function Home() {  
+  return (
+    <main className="flex h-full flexcol items-center justify-center
+    bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+    <div className='space-y-6 text-center' >
+       <h1 className='text-6xl font-semibold text-white drop-shadow-md'>
+         Auth
+       </h1>
+       <p className='text-white text-lg'>
+          A simple authentication service
+       </p>
+       <div>
+        <LoginButton>
+         <Button variant='secondary' size="lg">
+         Sign in
+         </Button>
+         </LoginButton>
+       </div>
+    </div>
+    
+      
+    </main> 
+  );
+}
